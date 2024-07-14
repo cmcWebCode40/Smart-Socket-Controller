e509b2b707b0c1de44870d681439e798e75a34c4
chinweikemichael <infinity_michael@yahoo.com>
2024-07-14 03:43:29 +0100

    featL: setup BLE library

--- App.tsx
+++ App.tsx
@@ -4,14 +4,19 @@ import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {RootNavigator} from '@/navigation';
+import FlashMessage from 'react-native-flash-message';
+import {BluetoothContextProvider} from '@/libs/context';
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
-        <StatusBar barStyle={'default'} />
-        <RootNavigator />
-      </NavigationContainer>
+      <BluetoothContextProvider>
+        <NavigationContainer>
+          <StatusBar barStyle={'default'} />
+          <RootNavigator />
+        </NavigationContainer>
+        <FlashMessage position="top" />
+      </BluetoothContextProvider>
     </SafeAreaProvider>
   );
 }
--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Smart Home Energy Management System
 
-
 ## Getting Started
 
 >**Note**: Make sure you have completed the [React Native - Environment Setup](https://reactnative.dev/docs/environment-setup) instructions till "Creating a new application" step, before proceeding.
@@ -18,7 +17,6 @@ npm start
 # OR using Yarn
 yarn start
 ```
-
 ## Step 2: Start your Application
 
 Let Metro Bundler run in its _own_ terminal. Open a _new_ terminal from the _root_ of your React Native project. Run the following command to start your _Android_ or _iOS_ app:
